81757fa6ce57fa5064e8b3ea11325274d5359e07
Lauri Rooden <lauri@rooden.ee>
2016-06-29 17:19:15 +0300

    Show property names in different color

--- content.js
+++ content.js
@@ -8,16 +8,16 @@
 
 	function draw(str, to, first) {
 		var match, val, err
-		, re = /(")(?:\\?.)*?"|-?[\d.]+|true|false|null|[[\]{},:]|(\S[^-[\]{}.:"\d]*)/g
+		, re = /("(?:\\?.)*?")\s*(:?)|-?[\d.]+|true|false|null|[[\]{},]|(\S[^-[\]{}."\d]*)/g
 		, node = document.createElement("div")
 		, span = document.createElement("span")
 		, comm = document.createElement("i")
+		, colon = document.createTextNode(": ")
 		, path = []
 		, cache = {
 			"{": fragment("{", "}"),
 			"[": fragment("[", "]"),
-			",": fragment(","),
-			":": document.createTextNode(": ")
+			",": fragment(",")
 		}
 
 		function fragment(a, b) {
@@ -66,17 +66,17 @@
 							node.parentNode.removeChild(node)
 						}
 						node = path.pop()
-					} else if (val == ":") {
-						node.appendChild(cache[val].cloneNode())
 					} else if (val == ",") {
 						node.len += 1
 						node.appendChild(cache[val].cloneNode(true))
 					} else {
 						tmp = span.cloneNode()
-						tmp.textContent = val
-						tmp.className = match[1] ? "c2": match[2] ? "c3" : "c1"
+						tmp.textContent = match[1] || val
+						tmp.className = match[2] ? "c2": match[1] ? "c1": match[3] ? "c3" : "c4"
 						node.appendChild(tmp)
 						if (match[2]) {
+							node.appendChild(colon.cloneNode())
+						} else if (match[3]) {
 							validate()
 						}
 					}
@@ -116,7 +116,7 @@
 		chr = str.charAt(0)
 		if (chr == "{" || chr == "[") {
 			var tag = document.createElement("style")
-			tag.textContent = 'h3{margin:1em}div{margin-left:4px;padding-left:1em;border-left:1px dotted #ccc;font:13px monospace}body>div{border:none}i{cursor:pointer;color:#ccc}i:hover{color:#999}i:before{content:" ▼ "}i.is-collpsed:before{content:" ▶ "}i:after{content:attr(data-content)}i.is-collpsed+div{display:none}.c1{color:#11c}.c2{color:#192}.c3{color:#f12;font-weight:bold}'
+			tag.textContent = 'h3{margin:1em}div{margin-left:4px;padding-left:1em;border-left:1px dotted #ccc;font:13px monospace}body>div{border:none}i{cursor:pointer;color:#ccc}i:hover{color:#999}i:before{content:" ▼ "}i.is-collpsed:before{content:" ▶ "}i:after{content:attr(data-content)}i.is-collpsed+div{display:none}.c1{color:#293}.c2{color:#66d}.c3{color:#f12}.c4{color:#10c}.c3,.c4{font-weight:bold}'
 			document.head.appendChild(tag)
 			draw(str, body, first)
 		}
