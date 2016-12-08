6355af750f34f6659dda34a0d2ab294117fe727c
Lauri Rooden <lauri@rooden.ee>
2016-12-08 23:27:23 +0200

    Remove pointer-events so it works on IE

--- content.js
+++ content.js
@@ -195,9 +195,9 @@
 		)
 	) {
 		var tag = document.createElement("style")
-		tag.textContent = 'h3{margin:1em}div{margin-left:4px;padding-left:1em;border-left:1px dotted #ccc;font:13px Menlo,monospace;pointer-events:none}'
+		tag.textContent = 'h3{margin:1em}div{margin-left:4px;padding-left:1em;border-left:1px dotted #ccc;font:13px Menlo,monospace}'
 		+ 'body>div{border:none}a{color:inherit;text-decoration:none}a:hover,a:focus{text-decoration:underline}'
-		+ 'a,i{pointer-events:auto}i{cursor:pointer;color:#ccc}.hi,i:hover{text-shadow: 1px 1px 3px #999;color:#333}'
+		+ 'i{cursor:pointer;color:#ccc}.hi,i:hover{text-shadow: 1px 1px 3px #999;color:#333}'
 		+ 'i:before{content:" ▼ "}i.is-collpsed:before{content:" ▶ "}i:after{content:attr(data-content)}i.is-collpsed+div{display:none}'
 		+ '.c1{color:#293}.c2{color:#66d}.c3{color:#f12}.c4{color:#10c}.c3,.c4{font-weight:bold}'
 		document.head.appendChild(tag)
